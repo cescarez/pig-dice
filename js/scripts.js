f8b07dc9fa057e72d6f30370e253211d752c74c7
Christabel Escarez and Nicole Cox <example@example.com>
2018-10-22 19:27:58 -0700

    This will add in columns for player one and player two.

--- js/scripts.js
+++ js/scripts.js
@@ -13,30 +13,45 @@ var playerTwo = {
   heldSum: 0,
 }
 
+// var game = {
+//   playerOne:
+// }
+
 Array.prototype.randomRoll = function () {
   return this[Math.floor(Math.random() * this.length)]
 };
 
 var dice = [1, 2, 3, 4, 5, 6];
-var currentPlayer = 0;
+var currentPlayer = 1;
+var heldSum = 0;
 
 //user logic
 $(document).ready(function() {
-  $("#roll").click(function(event) {
+  $("#roll1,roll2").click(function(event) {
     event.preventDefault();
-
     var currentRoll = dice.randomRoll()
     heldSum += currentRoll;
     console.log(heldSum);
 
-    $("#output").text(currentRoll);
-    // var playerOne = new Player()
 
+    // var playerOne = new Player()
+    if ( currentPlayer %2 === 0) {
+      $("#output2").text(currentRoll);
+
+      console.log("playerTwo")
+    } else if (currentPlayer %2 !== 0) {
+      console.log("playerone")
+      $("#output1").text(currentRoll);
+
+
+    }
 
   });
   $("#hold").click(function(event) {
     event.preventDefault();
-  currentPlayer += 1;
-  console.log(currentPlayer)
+    currentPlayer += 1;
+
+
+
  });
 });

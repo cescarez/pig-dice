547e2f8a38acb64d882a80dfdbf24b898e2177da
Christabel Escarez <christabel.escarez@gmail.com>
2018-10-23 08:40:06 -0700

    refactor dice check and storing turn score to total held

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-// function Player(currentRoll, holdSum) {
-//   this.currentRoll = currentRoll;
-//   this.heldSum = heldSum;
-// }
-
 var playerOne = {
   totalHeldSum: 0,
 }
@@ -23,24 +18,48 @@ var dice = [1, 2, 3, 4, 5, 6];
 var currentPlayer = 1;
 var turnSum = 0;
 
+var checkRoll = function(currentRoll) {
+  if (currentRoll === 1) {
+    turnSum = 0;
+    game.tempTurnSum = turnSum;
+    $("#output1").text("You lose your turn points! " + currentRoll + " :(");
+
+  } else {
+    turnSum += currentRoll;
+    console.log(turnSum);
+    game.tempTurnSum = turnSum;
+    $("#output1").text(currentRoll);
+  }
+}
+
+var addTurntoHeld = function () {
+  if (currentPlayer % 2 === 0) {
+    console.log("Add turn sum to player 1");
+    playerOne.totalHeldSum += game.tempTurnSum;
+    $("#total-sum1").text("Total Held Sum: " + playerOne.totalHeldSum);
+    if (playerOne.totalHeldSum >= 100) {
+      $("#total-sum1").text("Total Held Sum: " + playerOne.totalHeldSum + ". You're the winner!");
+    }
+  } else if (currentPlayer % 2 !== 0) {
+    console.log("Add turn sum to player 2");
+    playerTwo.totalHeldSum += game.tempTurnSum;
+    $("#total-sum2").text("Total Held Sum: " + playerTwo.totalHeldSum);
+    if (playerTwo.totalHeldSum >= 100) {
+      $("#total-sum2").text("Total Held Sum: " + playerTwo.totalHeldSum + ". You're the winner!");
+    }
+  }
+}
+
 //user logic
 $(document).ready(function() {
   $("#roll1").click(function(event) {
     event.preventDefault();
     var currentRoll = dice.randomRoll()
 
-    if (currentRoll === 1) {
-      turnSum = 0;
-      alert("You lose your turn points!")
-      game.tempTurnSum = turnSum;
-    } else {
-      turnSum += currentRoll;
-      console.log(turnSum);
-      game.tempTurnSum = turnSum;
-    }
+    checkRoll(currentRoll);
+
     if (currentPlayer % 2 !== 0) {
       console.log("playerone")
-      $("#output1").text(currentRoll);
       $("#turn-sum1").text("Current Turn Sum: " + turnSum);
       $("#turn-sum2").text("");
       $("#player-one-title").addClass("blue");
@@ -51,35 +70,15 @@ $(document).ready(function() {
     event.preventDefault();
     currentPlayer += 1;
 
-    if (currentPlayer % 2 === 0) {
-      console.log("Add turn sum to player 1");
-      playerOne.totalHeldSum += game.tempTurnSum;
-      $("#total-sum1").text("Total Held Sum: " + playerOne.totalHeldSum);
-      if (playerOne.totalHeldSum >= 100) {
-        $("#total-sum1").text("Total Held Sum: " + playerOne.totalHeldSum + ". You're the winner!");
-      }
-    } else if (currentPlayer % 2 !== 0) {
-      console.log("Add turn sum to player 2");
-      playerTwo.totalHeldSum += game.tempTurnSum;
-      $("#total-sum2").text("Total Held Sum: " + playerTwo.totalHeldSum);
-      if (playerTwo.totalHeldSum >= 100) {
-        $("#total-sum2").text("Total Held Sum: " + playerTwo.totalHeldSum + ". You're the winner!");
-      }
-    }
+    addTurntoHeld();
+
  });
  $("#roll2").click(function(event) {
    event.preventDefault();
    var currentRoll = dice.randomRoll()
 
-   if (currentRoll === 1) {
-     turnSum = 0;
-     game.tempTurnSum = turnSum;
-     alert("You lose your turn points!")
-   } else {
-     turnSum += currentRoll;
-     console.log(turnSum);
-     game.tempTurnSum = turnSum;
-   }
+   checkRoll(currentRoll);
+
 
    if (currentPlayer % 2 === 0) {
      $("#output2").text(currentRoll);
@@ -95,20 +94,7 @@ $(document).ready(function() {
    event.preventDefault();
    currentPlayer += 1;
 
-   if (currentPlayer % 2 === 0) {
-     console.log("Add turn sum to player 1");
-     playerOne.totalHeldSum += game.tempTurnSum;
-     $("#total-sum1").text("Total Held Sum: " + playerOne.totalHeldSum);
-     if (playerOne.totalHeldSum >= 100) {
-       $("#total-sum1").text("Total Held Sum: " + playerOne.totalHeldSum + ". You're the winner!");
-     }
-   } else if (currentPlayer % 2 !== 0) {
-     console.log("Add turn sum to player 2");
-     playerTwo.totalHeldSum += game.tempTurnSum;
-     $("#total-sum2").text("Total Held Sum: " + playerTwo.totalHeldSum);
-     if (playerTwo.totalHeldSum >= 100) {
-       $("#total-sum2").text("Total Held Sum: " + playerTwo.totalHeldSum + ". You're the winner!");
-     }
-   }
+   addTurntoHeld();
+
 });
 });

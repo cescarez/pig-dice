fe1eaa4aa7c1b437a624593f6fd77a7c78ac9d61
Christabel Escarez <christabel.escarez@gmail.com>
2018-10-24 13:43:48 -0700

    add message when 1 is rolled, to disappear on next roll

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,6 @@ function Player() {
   this.totalHeldSum = 0;
 }
 
-// var playerOne = {
-//   totalHeldSum: 0,
-// }
-//
-// var playerTwo = {
-//   totalHeldSum: 0,
-// }
-//
-// var game = {
-//   tempTurnSum: 0,
-// }
-
 Player.prototype.checkRoll = function(currentRoll, turnSum) {
   if (currentRoll === 1) {
     this.tempTurnSum = 0;
@@ -29,7 +17,7 @@ Player.prototype.checkRoll = function(currentRoll, turnSum) {
 Player.prototype.addTurntoHeld = function(turnSum) {
   this.totalHeldSum += turnSum;
   if (this.totalHeldSum >= 100) {
-    $("output-sentence").addClass("green").text("You're the winner!")
+    $("sentence-output").addClass("green").text("You're the winner!")
   }
 
 }
@@ -48,11 +36,15 @@ $(document).ready(function() {
   var playerTwo = new Player()
   $("#roll").click(function(event) {
     event.preventDefault();
-    $("output-sentence").text="clear"
+    $("#sentence-output").removeClass().text("")
     var currentRoll = dice.randomRoll();
-;
-
-    turnSum += currentRoll;
+//entire section to be refactored
+    if (currentRoll === 1) {
+      turnSum = 0;
+      $("#sentence-output").addClass("red-bg").text("You lose your turn points! :( Next player's turn!");
+    } else {
+      turnSum += currentRoll;
+    }
     if (currentPlayer % 2 !== 0) {
       console.log("playerone");
       playerOne.currentRoll = currentRoll;
@@ -75,15 +67,11 @@ $(document).ready(function() {
     $("#turn-sum2").text("");
     $("#output1").text("");
     $("#output2").text("");
-    // playerOne.tempTurnSum = 0;
-    // playerTwo.tempTurnSum = 0;
 
     currentPlayer += 1;
 
     console.log("Current player count: " + currentPlayer);
     console.log("Current player mod 2: " + currentPlayer%2);
-
-//both cases execute
 
     if (currentPlayer % 2 === 0) {
       playerOne.addTurntoHeld(turnSum);
@@ -100,23 +88,5 @@ $(document).ready(function() {
     }
 
     turnSum = 0;
-
-// //both cases execute
-//     switch(currentPlayer % 2) {
-//       case 1:
-//       playerOne.addTurntoHeld(turnSum);
-//       $("#player-two-title").addClass("blue");
-//       $("#player-one-title").removeClass();
-//       console.log("Add turn sum to player 1");
-//       $("#total-sum1").text("Player One Total Held Sum: " + playerOne.totalHeldSum);
-//       break;
-//       case 0:
-//       playerTwo.addTurntoHeld(turnSum);
-//       $("#player-one-title").addClass("blue");
-//       $("#player-two-title").removeClass();
-//       console.log("Add turn sum to player 2");
-//       $("#total-sum2").text("Player Two Total Held Sum: " + playerTwo.totalHeldSum);
-//       break;
-//     }
  });
 });
